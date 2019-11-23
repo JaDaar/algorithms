d815f3a93d82d495227c49d34e9dc8745ead2d56
Jeffrey Hamlin <sourcesage@gmail.com>
2019-11-23 10:07:33 -0500

    Add project files.

---
+++ Sort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 7,6,5,1 };
+            var b=InsertionSort(x);
+            Console.WriteLine(b);
+        }
+
+        private static int[] InsertionSort(int[] list)
+        {
+            int key;
+            for(var iter = 0; iter < list.Length; iter++)
+            {
+                for (var index = 0; index < list.Length; index++)
+                {
+                    key = list[index];
+                    if (index + 1 < list.Length)
+                    {
+                        while (list[index + 1] < key)
+                        {
+                            var shiftDown = list[index + 1];
+                            list[index] = shiftDown;
+                            list[index + 1] = key;
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
